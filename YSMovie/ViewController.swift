1edbb84d83e6990ef7a589925a83bbfefb1a2e5a
Yuri Strack <yuri.strack@ingresso.com>
2024-01-07 11:57:31 -0300

    Initial Commit

---
+++ YSMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YSMovie
+//
+//  Created by ystrack on 07/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
